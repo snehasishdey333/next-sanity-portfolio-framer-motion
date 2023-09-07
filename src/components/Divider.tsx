beeeac852cd734fbcb75fcb981b0e1bc9f698565
Snehasish Dey <snehasishdey333@gmail.com>
2023-09-07 18:05:20 +0530

    added motion to divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import Motion from './Motion'
 
 type Props = {}
 
 const Divider = (props: Props) => {
   return (
-    <div className='w-sccreen'>
+    <Motion delay={1.5} direction='left'>
+      <div className='w-screen'>
         <div className='bg-violet-500 w-[40%] h-[1px]'>
         </div>
     </div>
+    </Motion>
+    
   )
 }
 
